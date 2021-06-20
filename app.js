b940f6fe9917acd2e60a60cb7081bd70bfd2c271
jasony93 <jasony9393@gmail.com>
2021-06-20 17:04:34 +0900

    second commit

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const port = process.env.port || 3000;
 
 app.get('/', (req, res) => {
     res.send("Welcome to node js server");
 });
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("node js!")
 })
