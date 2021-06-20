ea407c0d32e733cc9ad823b78417f65ba3fb17ac
jasony93 <jasony9393@gmail.com>
2021-06-20 20:01:55 +0900

    cors option added

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.port || 3000;
 const fs = require('fs');
 
+
 //통합종목시세 다운로드 링크
 //https://data.krx.co.kr/contents/MDC/main/downloadMenu.cmd?type=tot
+
+
+
+const corsOptions = {
+    origin: 'http://localhost:3000', // 허락하고자 하는 요청 주소
+    credentials: true, // true로 하면 설정한 내용을 response 헤더에 추가 해줍니다.
+};
+
+app.use(cors(corsOptions));
 
 app.get('/:id', (req, res) => {
 
@@ -20,7 +31,7 @@ app.get('/:id', (req, res) => {
         }
     }
 
-    res.header("Access-Control-Allow-Origin", "*");
+    // res.header("Access-Control-Allow-Origin", "*");
     res.json(object)
 });
 
